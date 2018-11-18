2ec835d806686cd995dcf9c9390b545fd97af76b
Albert <fajb420@gmail.com>
2018-11-18 16:18:22 +0300

    create AuthService

---
+++ VkXamarinApp/VkXamarinApp/Services/Auth/AuthService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+[assembly: Xamarin.Forms.Dependency(typeof(VkXamarinApp.Services.Auth.AuthService))]
+namespace VkXamarinApp.Services.Auth
+{
+    class AuthService : IAuthService
+    {
+        public bool IsAuthoriz()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
